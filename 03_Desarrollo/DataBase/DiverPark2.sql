8f48366ea8168408791eaa836adccfe8eaeb934e
Jhon Jainer <jjchavez44@misena.edu.co>
2019-06-12 22:27:59 -0500

    Chore: se carga la base de dato
    se sube script de la construccion de la base de datos
    se insertan datos
    se muesta el diagrama entidad relacion

--- 03_Desarrollo/DataBase/DiverPark2.sql
+++ 03_Desarrollo/DataBase/DiverPark2.sql
@@ -1,9 +1,18 @@
-## drop database diverpark;
+
 create database diverpark;
 use diverpark;
+
+create table tipo_documento
+(
+    id_tipodoc VARCHAR(10),
+	siglas VARCHAR(5)  not null,
+	nombre_tipo_doc VARCHAR(45)  not null
+);
+
 create table usuario
 (
     numero_documento varchar(25),
+    fk_id_tipodoc VARCHAR(10),
     primer_nombre varchar(25) not null,
     segundo_nombre varchar(25),
     primer_apellido varchar(25) not null,
@@ -11,50 +20,52 @@ create table usuario
     departamento varchar(25)not null,
     ciudad varchar (25) not null,
     direccion varchar(25) not null,
+    avatar blob,
     email VARCHAR (30) unique not null,
     contraseña VARCHAR(15) not null,
-    telefono bigint,
+    telefono bigint not null,
     fk_id_rol VARCHAR (10),
-    fk_id_documento VARCHAR(10),
     fk_id_producto int
-);
-
-create table tipo_documento
-(
-    id_documento VARCHAR(10),
-	siglas VARCHAR(5)  not null,
-	nombre_tipo_doc VARCHAR(30)  not null
 );
 
 create table rol
 (
     id_rol VARCHAR(10),
     nombre_rol VARCHAR (25)  not null,
-    descripcion varchar(50)
+    descripcion varchar(150)
 );
-
 
 create table tipo_producto
 (
 	id_producto int primary key,
     nombre_producto varchar (25) not null,
+    imagen_park blob,
     tamaño varchar(30) not null,
-    descripcion varchar(50) not null,
+    descripcion varchar(100) not null,
     precio float not null,
     fk_id_numero_documento varchar (25),
-    fk2_id_documento varchar (10)
+    fk2_id_tipodoc varchar (10)
     );
     
     
     create table servicio
 (
 	id_servicio int primary key,
-    nombre_servicio varchar (25) not null,
+    nombre_servicio varchar (50) not null,
 	fecha date not null,
     direccion varchar (25) not null,
-	observacion varchar (50),
+	observacion varchar (150),
     fk_id_producto int
-    
+);
+
+create table tipo_pago
+(
+    id_tpago int primary key,
+    siglas varchar (5),
+    nombre_tipo_pago VARCHAR(30)  not null
+	/*fk_id_tarjeta_c VARCHAR(10),
+    fk_id_tarjeta_d VARCHAR(10),
+    fk_id_giro VARCHAR(10)*/
 );
 
 create table factura
@@ -68,57 +79,52 @@ create table factura
     sub_total float  not null, 
     total float  not null, 
     fecha datetime not null,
-	fk_id_tpago varchar (10),
+	fk_id_tpago int,
     fk_id_producto int
 );
 
 
-create table tipo_pago
-(
-    id_tpago VARCHAR (10) primary key,
-    nombre_tipo_pago VARCHAR(30)  not null,
-    fk_id_plan VARCHAR(10),
-	fk_id_tarjeta_c VARCHAR(10),
-    fk_id_tarjeta_d VARCHAR(10),
-    fk_id_giro VARCHAR(10)
-);
-
 create table tarjeta_credito
 (
-    id_tarjeta_c VARCHAR(10),
+    id_tarjeta_c int,
     nombre VARCHAR(25) not null,
     apellido VARCHAR(25)  not null,
     nombre_banco varchar (25) not null,
     numero_de_tarjeta BIGINT not null,
     codigo_seguridad int not null,
-    fecha_vencimiento date not null
+    fecha_vencimiento date not null,
+    fk_id_tpago int
 );
 
 create table tarjeta_debito
 (
-    id_tarjeta_d VARCHAR(10),
+    id_tarjeta_d int,
     nombre VARCHAR(25) not null,
     apellido VARCHAR(25) not null,
     nombre_banco varchar (25) not null,
     numero_de_tarjeta BIGINT not null,
     codigo_seguridad int not null,
-    fecha_vencimiento date not null
+    fecha_vencimiento date not null,
+    fk_id_tpago int
 );
 
 create table giro_empresarial
 (
-    id_giro VARCHAR (10),
+    id_giro int,
     nombre_titular varchar (25)not null,
     apellido_titular varchar (25)not null,
     documento_titular varchar (25)not null,
     numero_telefono BIGINT not null,    
-    valor_giro BIGINT not null
+    valor_giro float not null,
+    fk_id_tpago int
 );
 
 
 create table envio
 (
 	id_envio int auto_increment primary key,
+    departamento varchar(25)not null,
+    ciudad varchar (25) not null,
     direccion_envio varchar (30) not null,
     fecha_envio date not null,
     fk_id_factura int
@@ -159,20 +165,22 @@ create table servidor_correo
 
 -- =================== AGREGAR LLAVES PRIMARIAS FORANEAS Y RELACIONES================
 
--- se definen llaves primarias de tipo_documento y usuario, tambien foranea usuario -tipo_ocumento
-ALTER TABLE tipo_documento ADD primary key (id_documento);
-ALTER TABLE usuario ADD constraint FOREIGN KEY (fk_id_documento)REFERENCES tipo_documento(id_documento);
-ALTER TABLE usuario ADD primary key (numero_documento, fk_id_documento);
+-- se definen llaves primarias de tipo_documento y usuario, tambien foranea usuario -tipo_documento
+ALTER TABLE tipo_documento ADD primary key (id_tipodoc);
+ALTER TABLE usuario ADD constraint FOREIGN KEY (fk_id_tipodoc)REFERENCES tipo_documento(id_tipodoc);
+ALTER TABLE usuario ADD primary key (numero_documento, fk_id_tipodoc);
 
 -- rol definen las  llaves primaria y foranea de la tabla 
 ALTER TABLE rol ADD primary key (id_rol);
 ALTER TABLE usuario ADD constraint FOREIGN KEY (fk_id_rol)REFERENCES rol(id_rol);
 
--- parque - usuario  definen las  llaves primaria y foranea de la tabla 
-alter table tipo_producto  add constraint foreign key (fk_id_numero_documento,fk2_id_documento ) references usuario(numero_documento,fk_id_documento );
+--  - tipo_producto   define las  llaves  foraneas de la tabla 
+alter table tipo_producto  add constraint foreign key (fk_id_numero_documento,fk2_id_tipodoc) references usuario(numero_documento,fk_id_tipodoc );
 
+--  - factura - tipo producto  define las  llaves  foraneas de la tabla 
 alter table factura add constraint foreign key (fk_id_producto) references tipo_producto(id_producto);
 
+--  - servicio - tipo producto   define las  llaves  foraneas de la tabla 
 alter table servicio add constraint foreign key (fk_id_producto) references tipo_producto(id_producto);
 
 -- tipo_pago- factura  definen las  llaves primaria y foranea de la tabla 
@@ -181,15 +189,15 @@ alter table factura add constraint foreign key (fk_id_tpago) references tipo_pag
 
 -- tarjeta_credito  definen las  llaves primaria y foranea de la tabla 
 ALTER TABLE tarjeta_credito ADD primary key (id_tarjeta_c);
-ALTER TABLE tipo_pago ADD constraint FOREIGN KEY (fk_id_tarjeta_c)REFERENCES tarjeta_credito(id_tarjeta_c);
+ALTER TABLE tarjeta_credito ADD constraint FOREIGN KEY (fk_id_tpago) references tipo_pago (id_tpago);
 
 -- tarjeta_debito definen las  llaves primaria y foranea de la tabla
 ALTER TABLE tarjeta_debito ADD primary key (id_tarjeta_d);
-ALTER TABLE tipo_pago ADD constraint FOREIGN KEY (fk_id_tarjeta_d)REFERENCES tarjeta_debito (id_tarjeta_d);
+ALTER TABLE tarjeta_debito ADD constraint FOREIGN KEY (fk_id_tpago) references tipo_pago (id_tpago);
 
 -- efectivo definen las  llaves primaria y foranea de la tabla
 ALTER TABLE giro_empresarial ADD primary key (id_giro);
-ALTER TABLE tipo_pago ADD constraint FOREIGN KEY (fk_id_giro)REFERENCES giro_empresarial(id_giro);
+ALTER TABLE giro_empresarial ADD constraint FOREIGN KEY (fk_id_tpago) references tipo_pago (id_tpago);
 
 -- envio definen las  llaves primaria y foranea de la tabla
 ALTER TABLE envio ADD constraint FOREIGN KEY (fk_id_factura)REFERENCES factura(id_factura);
---
+++ 03_Desarrollo/DataBase/insercion datos.sql
@@ -0,0 +1,102 @@
+-- drop database diverpark;
+use diverpark;
+
+-- Inserción de datos tabla tipo_documento
+
+insert into tipo_documento -- (id_tipodoc, siglas, nombre_tipo_doc)
+values 
+('1', 'CC', 'cedula de ciudadania'),
+('2', 'CE', 'cedula de extranjera'),
+('3', 'NIT', 'numero identificacion tributaria'),
+('4', 'PPS', 'tarjeta pasaporte');
+
+-- Inserción de datos tabla usuario
+insert into rol -- (id_rol, nombre_rol, descripcion)
+values 
+('1', 'administrador', 'el administrador podra crear, eliminar o modificar ya sean productos, servicios o usuarios'),
+('2', 'cliente', 'el cliente podra visualizar productos crear su usuario y modificarlo ademas podra adquirir los productos y servicios ofrecidos por la empresa'),
+('3', 'visitante', 'el visitante podra solo visualizar los productos y servicios');
+
+-- Inserción de datos tabla usuario
+
+insert into usuario -- (numero_documento, primer_nombre, segundo_nombre, primer_apellido, segundo_apellido, departamento, ciudad, direccion, email, contraseña, telefono, fk_id_rol, fk_id_documento, fk_id_producto)
+values 
+('1013614965', '1', 'nicole', 'camila', 'ramirez', 'montero', 'cundinamarca', 'zipaquira', 'cra. 79# 57-86',null, 'ncamila@gmail.com', 'buybdf***5', 7584956,'2',1),
+('1030525932', '3', 'Cristian', 'David', 'Zapata', 'Moreno', 'cundinamarca', 'Bogata', 'cra. 67# 38-86',null, 'criszapata@gmail.com', 'tyrea827eh', 5973329,'2',1),
+('39665219', '4', 'Luz', 'Edilma', 'Perez', 'Vargas', 'Antioquia', 'Medellin', 'cll. 18# 56-76',null, 'Lucesita23@gmail.com', 'byryaqo', 7810082,'2',1),
+('6034072', '1', 'Pablo', 'Deibi', 'Rincon', 'Rodriguez', 'tolima', 'Chaparral', 'transv. 4# 10-85',null, 'pablito2672@gmail.com', 'thanyrwip', 2333213,'2',1),
+('jng614693', '4', 'Jhohana', 'Patricia', 'Maldonado', 'Suarez', 'Caqueta', 'Florencia', 'Diag. 54# 23-45',null, 'jhpatricia@gmail.com', 'uytvhgdfs5', 5903615,'2',1),
+('1013414265', '2', 'Andres', 'Yesid', 'Rios', 'Sanchez', 'casanare', 'yopal', 'cra. 24# 65-04',null, 'yiyo234@gmail.com', 'buf**uyhsbb', 5735218,'2',1),
+('1032178543', '2', 'Heidy', 'Julied', 'Moreno', 'Niampira', 'sucre', 'Sincelejo', 'cra. 27# 34-16',null, 'julied1232@gmail.com', 'ytrgujs***5', 7427189,'2',1),
+('1013724962', '2', 'Jeison', 'David', 'Marquez', 'Gomez', 'Cordoba', 'Monteria', 'cra. 39# 17-83',null, 'jdmarquez@gmail.com', 'bytbf***5', 6789546,'2',1),
+('1012534897', '1', 'Mary', 'Elizabeth', 'Orjuela', 'Moreno', 'valle del cauca', 'cali', 'transv. 49# 67-49',null, 'meormo.245@gmail.com', 'nursf***5', 3476523,'2',1),
+('38666852', '3', 'Juan', '', 'Alban', 'Giraldo', 'Tolima', 'Ibague', 'cra. 65# 42-33',null, 'juanitoalgi@gmail.com', 'jjuantye', 7549904,'2',1),
+('1025476897', '1', 'Laura', 'Vanessa', 'Rojas', '', 'cundinamarca', 'Girardot', 'dig. 67# 87-73',null, 'Laurisvanr@gmail.com', '2789unealkj', 2357682,'2',1),
+('1056825762', '1', 'Ronald', 'Alfredo', 'Leiton', 'Hurtado', 'Risaralda', 'Pereira', 'cra. 68 cbis# 37 a-96',null, 'Ronaldleiton@gmail.com', 'kshuitytabj', 6820056,'2',1),
+('e-25614965', '3', 'Leidy', 'Johanna', 'Rozo', 'Cubillos', 'Caldas', 'Manizales', 'cra. 21 sur# 67-79',null, 'cubilloslj@gmail.com', 'yegtreqw3456', 8643267,'2',1),
+('1065789076', '2', 'camilo', '', 'Gomez', '', 'Boyaca', 'Tunja', 'cll. 32# 97-64 sur',null, 'camiGomez4532@gmail.com', 'cristiabyv', 5987356,'2',1),
+('1013654762', '1', 'Angie', 'Liliana', 'Zambrano', 'Suares', 'Tolima', 'Melgar', 'calle. 02 este# 67-63',null, 'anlilianita.217@gmail.com', 'Liliana2765', 3176542,'2',1);
+
+insert into tipo_producto
+values
+(01,'Parques de madera', null, '25 * 5 *8', 'thjuf ebdfbddhgaau cfhhudr', 1.000000,'jng614693', '4'),
+(02,'Parques Metalicos', null, '20 *5 * 5 *8', 'thjuf ebdfbddhgaau cfhhudr', 2.001000,'e-25614965','3'),
+(03,'Parques Biosaludables', null, '10 * 5 *8 *8', 'uygtdfhffdds  vhgfdj  tf de frg jhgu ', 1.000000,'1065789076', '2'),
+(04,'Parques publicos', null, '5 * 5 *5 *5', 'thjuf ebdfbddhgaau cfhhudr', 1.465000,'1013654762','1');
+
+insert into servicio 
+values
+(01, 'instalacion de parques','2019-03-31', 'cra. 21 sur# 67-79','  ',02),
+(02, 'mantenimiento de parques','2019-03-31', 'cra. 21 sur# 67-79','',03);
+
+
+insert into tipo_pago
+values
+('01','TC', 'tarjeta credito'),
+('02','TC', 'tarjeta debito'),
+('03','GE', 'giro_empresarial');
+
+insert into factura
+values
+(0001, 'p-02','Parques Metalicos', 3, 1805.000, 205.000, 5415.000, 6300.000, '2019-08-31 16:19:59', 01, 02),
+(0002, 'p-02','Parques Metalicos', 1, 1805.000, 205.000, 1805.000, 2001.000, '2020-02-11 17:19:59', 01, 02),
+(0003, 'p-01','Parques de madera', 2, 910.000, 90.000, 1810.000, 2000.000, '2019-08-31 06:19:59', 03, 01),
+(0004, 'p-03','Parques Biosaludables', 2, 950.000, 50.000, 1900.000, 2000.000, '2019-08-31 10:19:59', 03, 03),
+(0005, 'p-01','Parques de madera', 4, 910.000, 90.000, 364000.000, 4000.000, '2019-08-31 10:19:59', 02, 03),
+(0006, 'p-01','Parques de madera', 4, 910.000, 90.000, 364000.000, 4000.000, '2019-08-31 10:19:59', 01, 03);
+
+insert into tarjeta_credito
+values 
+('01', 'juan', 'galindo', 'Banco de Bogota', 456213457865, 2561, '2019-03-31','02' ),
+('02', 'paola', 'acevedo','Banco caja social',  254861245367, 6543, '2018-07-11','01'),
+('03', 'michael', 'restrepo','Banco Bancolombia',  546123547852, 4318, '2018-04-20','03'),
+('04', 'gerardo', 'pardo','Banco Occidente',  542698234561, 5678,'2019-01-01','03'),
+('05', 'viviana', 'mendez','Banco BBVA',  456213457865, 7645, '2019-02-07','01');
+
+insert into tarjeta_debito
+values 
+('01', 'tobias', 'vargas','Banco caja social', 4512478634, 8765, '2019-03-31','03'),
+('02', 'juan pablo', 'ricaute','Banco de Bogota', 4532186547, 7652, '2019-06-24','01'),
+('03', 'falcao', 'vacca', 'Banco del Occidente', 9645213794, 3789, '2019-08-31','02'),
+('04', 'yimmy', 'duque','Banco BBVA', 6357219435, 6524, '2019-05-11','01'),
+('05', 'gabriela', 'rios','Banco Bancolombia',  463185475, 8675, '2019-04-15','02');
+
+insert into giro_empresarial
+values 
+('01', 'Alberto jose', 'vargas', '4512478634', 0318765547, 100.000,'03'),
+('02', 'Alberto jose', 'vargas', '4512478634', 0318765547, 500.000,'03'),
+('03', 'Alberto jose', 'vargas', '4512478634', 0318765547, 800.000,'03'),
+('04', 'Alberto jose', 'vargas', '4512478634', 0318765547, 300.000,'03'),
+('05', 'Alberto jose', 'vargas', '4512478634', 0318765547, 300.000,'03');
+
+-- truncate table envio;
+insert into envio
+values
+(0001, 'cundinamarca', 'zipaquira', 'cra. 79# 57-86', '2019-08-31', 0001 ),
+(0002, 'Tolima', 'Ibague', 'cra. 65# 42-33', '2019-10-24', 0002 ),
+(0003, 'valle del cauca', 'cali', 'transv. 49# 67-49', '2019-09-01', 0003 ),
+(0004, 'Cordoba', 'Monteria', 'cra. 39# 17-83', '2020-01-01', 0004 ),
+(0005, 'Boyaca', 'Tunja', 'cll. 32# 97-64 sur', '2019-12-31', 0005 ),
+(0006, 'Tolima', 'Melgar', 'calle. 02 este# 67-63', '2019-07-04', 0006 );
+
+-- select * from envio;
