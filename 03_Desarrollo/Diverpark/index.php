c1fc5dbfc15cc3dc9c47bf2939a7ba90545da1a1
carlosPortilla <48883369+carlosPortilla@users.noreply.github.com>
2019-09-07 15:17:22 -0500

    chore: :<  Desarrollo inicial del proyecto diverpak
     Desarrollo inicial del proyecto diverpak

---
+++ 03_Desarrollo/Diverpark/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Diverpak</title>
+
+</head>
+<body>
+<P><b>JHON ME CHUPA EL PENE MIENTRAS GERMAN ME BESA LOS HUEBOS Y MICHEL CHUPA EN LA 22 CON CARACAS A TAN SOLO MIL EN PROMOCION</b></P>
+
+
+    
+</body>
+</html>
