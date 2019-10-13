0557d19f8a660e621724b69ed73b7b0676a2dd6c
Jhon Chavez <47484254+jhoncito1@users.noreply.github.com>
2019-10-13 08:40:03 -0500

    se crea una nueva rama para programar

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Diverpak</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+
+<header>
+    <?php
+      include('menu.php');
+    ?>
+ </header>
+<div class="content-all">
+    <div class="content-carrusel">
+        <figure><img src="img/A.jpg" alt=""></figure>
+        <figure><img src="img/A.jpg" alt=""></figure>
+        <figure><img src="img/B.jpg" alt=""></figure>
+        <figure><img src="img/C.jpg" alt=""></figure>
+        <figure><img src="img/D.jpg" alt=""></figure>
+        <figure><img src="img/E.jpg" alt=""></figure>
+        <figure><img src="img/F.jpg" alt=""></figure>
+        <figure><img src="img/G.jpg" alt=""></figure>
+        <figure><img src="img/H.jpg" alt=""></figure>
+        <figure><img src="img/I.jpg" alt=""></figure>
+        <figure><img src="img/J.jpg" alt=""></figure>
+        <figure><img src="img/J.jpg" alt=""></figure>
+        <figure><img src="img/J.jpg" alt=""></figure>
+        <figure><img src="img/J.jpg" alt=""></figure>
+    </div>
+ </div>
+</body>
+</html>
