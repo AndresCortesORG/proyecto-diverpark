7299eb45aa3a61390e6bb99613de989e1a78b777
Jhon Chavez <47484254+jhoncito1@users.noreply.github.com>
2019-11-27 22:35:55 -0500

    creando  v5
    creando version 5 de pagina web diverpark

---
+++ DP3/footer.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/footer.css">
+</head>
+<body>
+<footer>
+       
+       <div class="container-footer-all">
+        
+            <div class="container-body">
+
+                <div class="colum1">
+                    <h1>Mas informacion de la compañia</h1>
+
+                    <p>Esta compañia se dedica a la venta de software integrado con un 
+                    conjunto de cosas que no se lo que estoy escribiendo, este 
+                    texto es solo para llenara informacion en el cuadro de informacion 
+                    de la compañia.</p>
+
+                </div>
+
+                <div class="colum2">
+
+                    <h1>Redes Sociales</h1>
+
+                    <div class="row">
+                        <a href="http://facebook.com" target="_blank" >
+                        <img src="icon/facebook.png"></a>
+                        <label>Siguenos en Facebook</label>
+                    </div>
+                    <div class="row">
+                        <a href="http://twitter.com" target="_blank" rel="noopener noreferrer">
+                        <img src="icon/twitter.png"></a>
+                        <label>Siguenos en Twitter</label>
+                    </div>
+                    <div class="row">
+                        <a href="http://instagram.com" target="_blank" rel="noopener noreferrer">
+                        <img src="icon/instagram.png"></a>
+                        <label>Siguenos en Instagram</label>
+                    </div>
+                    <div class="row">
+                        <a href="http://pinterest.com" target="_blank" rel="noopener noreferrer">
+                        <img src="icon/pinterest.png"></a>
+                        <label>Siguenos en Pinteres</label>
+                    </div>
+
+
+                </div>
+
+                <div class="colum3">
+
+                    <h1>Informacion Contactos</h1>
+
+                    <div class="row2">
+                        <img src="icon/house.png">
+                        <label>Colombia <br>
+                        Bogota DC,<br>
+                        SENA<br>
+                        Sede colombia</label>
+                    </div>
+
+                    <div class="row2">
+                        <img src="icon/smartphone.png">
+                        <label>+57 3209988852</label>
+                    </div>
+
+                    <div class="row2">
+                        <img src="icon/contact.png">
+                         <label>servicios@diverpark.com</label>
+                    </div>
+
+                </div>
+
+            </div>
+        
+        </div>
+        
+        <div class="container-footer">
+               <div class="footer">
+                    <div class="copyright">
+                        © 2019 Todos los Derechos Reservados | <a href="">Diverpark</a>
+                    </div>
+
+                    <div class="information">
+                        <a href="">Informacion Compañia</a> | <a href="">Privacidad y Politica</a> | <a href="">Terminos y Condiciones</a>
+                    </div>
+                </div>
+
+            </div>
+        
+    </footer>
+</body>
+</html>
---
+++ DP3/index.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <title>DiverPark</title>
+  <link rel="stylesheet" href="css/estilos.css" />
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous" />
+</head>
+
+<body class="hidden">
+  <div class="centrado" id="onload">
+    <div class="lds-facebook">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+  <header>
+    <nav id="nav" class="nav1">
+      <div class="contenedor-nav">
+        <div class="logo">
+          <img src="img/logo1.png" alt="" />
+        </div>
+        <div class="enlaces" id="enlaces">
+          <a href="#" id="enlace-inicio" class="btn-header">Inicio</a>
+          <a href="#" id="enlace-servicio" class="btn-header">Nosotros</a>
+          <a href="#" id="enlace-trabajo" class="btn-header">Productos y Servicios</a>
+          <a href="#" id="enlace-equipo" class="btn-header">Equipo</a>
+          <a href="#" id="enlace-contacto" class="btn-header">Contacto</a>
+        </div>
+        <div class="icono" id="open">
+          <span>&#9776;</span>
+        </div>
+      </div>
+    </nav>
+    <div class="textos">
+      <h1>DiverPark.com</h1>
+      <h2>Agencia de parques de diversión</h2>
+    </div>
+  </header>
+  <main>
+    <section class="about" id="servicio">
+      <div class="contenedor">
+        <h3>Nuestros servicios</h3>
+        <p class="after">Siempre mejorando para ti</p>
+        <div class="servicios">
+          <div class="caja-servicios">
+            <img src="img/heart.png" alt="" />
+            <h4>Creativos y asombrosos</h4>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+          </div>
+          <div class="caja-servicios">
+            <img src="img/responsive.png" alt="" />
+            <h4>Creativos y asombrosos</h4>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+          </div>
+          <div class="caja-servicios">
+            <img src="img/efectos.png" alt="" />
+            <h4>Creativos y asombrosos</h4>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="work contenedor" id="trabajo">
+      <h3>Nuestro trabajo</h3>
+      <p class="after">No solo hacemos parques, creamos felicidad</p>
+      <div class="botones-work">
+        <ul>
+          <li class="filter" data-nombre="todos">De madera</li>
+          <li class="filter" data-nombre="diseño">Metalicos</li>
+          <li class="filter" data-nombre="programacion">Infantiles</li>
+          <li class="filter" data-nombre="marketing">Biosaludables</li>
+        </ul>
+      </div>
+      <div class="galeria-work">
+        <div class="cont-work programacion">
+          <div class="img-work">
+            <img src="img/programacion1.jpeg" alt="" />
+          </div>
+          <div class="textos-work">
+            <h4>Programacion</h4>
+          </div>
+        </div>
+        <div class="cont-work programacion">
+          <div class="img-work">
+            <img src="img/programacion2.jpeg" alt="" />
+          </div>
+          <div class="textos-work">
+            <h4>Programacion</h4>
+          </div>
+        </div>
+        <div class="cont-work programacion">
+          <div class="img-work">
+            <img src="img/programacion3.jpeg" alt="" />
+          </div>
+          <div class="textos-work">
+            <h4>Programacion</h4>
+          </div>
+        </div>
+        <div class="cont-work diseño">
+          <div class="img-work">
+            <img src="img/diseño1.jpeg" alt="" />
+          </div>
+          <div class="textos-work">
+            <h4>Diseño</h4>
+          </div>
+        </div>
+        <div class="cont-work diseño">
+          <div class="img-work">
+            <img src="img/diseño2.jpeg" alt="" />
+          </div>
+          <div class="textos-work">
+            <h4>Diseño</h4>
+          </div>
+        </div>
+        <div class="cont-work diseño">
+          <div class="img-work">
+            <img src="img/diseño3.jpeg" alt="" />
+          </div>
+          <div class="textos-work">
+            <h4>Diseño</h4>
+          </div>
+        </div>
+        <div class="cont-work marketing">
+          <div class="img-work">
+            <img src="img/marketing1.jpeg" alt="" />
+          </div>
+          <div class="textos-work">
+            <h4>Marketing</h4>
+          </div>
+        </div>
+        <div class="cont-work marketing">
+          <div class="img-work">
+            <img src="img/marketing2.jpeg" alt="" />
+          </div>
+          <div class="textos-work">
+            <h4>Marketing</h4>
+          </div>
+        </div>
+        <div class="cont-work marketing">
+          <div class="img-work">
+            <img src="img/marketing3.jpeg" alt="" />
+          </div>
+          <div class="textos-work">
+            <h4>Marketing</h4>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="team contenedor" id="equipo">
+      <h3>Nuestra empresa</h3>
+      <p class="after">somos una aeuipo de programadores capaces de desarrollar software eficiente y moderno, utilizando las mas actuales y mejores tecnologias</p>
+      <h3>Nuestro equipo</h3>
+      <div class="card">
+        <div class="content-card">
+          <div class="people">
+            <img src="img/jhonj.jpg" alt="" />
+          </div>
+          <div class="texto-team">
+            <h4>Jhon Chavez</h4>
+            <p>Programador Backend</p>
+          </div>
+        </div>
+        <div class="content-card">
+          <div class="people">
+            <img src="img/people2.jpg" alt="" />
+          </div>
+          <div class="texto-team">
+            <h4>Juan carlos Sanchez</h4>
+            <p>Diseñador</p>
+          </div>
+        </div>
+        <div class="content-card">
+          <div class="people">
+            <img src="img/people3.jpg" alt="" />
+          </div>
+          <div class="texto-team">
+            <h4>German Galindo</h4>
+            <p>Programador Frontend</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+
+  <footer id="contacto">
+    <?php
+    include 'footer.php';
+    ?>
+  </footer>
+
+  <script src="js/jquery.js"></script>
+  <script src="js/main.js"></script>
+  <script src="js/filtro.js"></script>
+</body>
+
+</html>
